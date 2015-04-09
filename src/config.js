38027df3084947d8b4488bcfb1da8ba50cf0fd16
Daniel Silva <daniel@developmentseed.org>
2015-04-09 17:15:05 +0100

    Setup tasks and config for demo

--- src/config.js
+++ src/config.js
@@ -1,12 +1,22 @@
 'use strict';
 
-module.exports.oauthKey = 'KcVfjQsvIdd7dPd1IFsYwrxIUd73cekN1QkqtSMd';
-module.exports.oauthSecret = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
+// module.exports.oauthKey = 'KcVfjQsvIdd7dPd1IFsYwrxIUd73cekN1QkqtSMd';
+// module.exports.oauthSecret = 'K7dFg6rfIhMyvS8cPDVkKVi50XWyX0ibajHnbH8S';
+module.exports.oauthKey = '5A043yRSEugj4DJ5TljuapfnrflWDte8jTOcWLlT';
+module.exports.oauthSecret = 'aB3jKq1TRsCOUrfOIZ6oQMEDmv2ptV76PA54NGLL';
+
 module.exports.accessToken = 'pk.eyJ1IjoiYWFyb25saWRtYW4iLCJhIjoiNTVucTd0TSJ9.wVh5WkYXWJSBgwnScLupiQ';
+
 module.exports.raven = 'https://e45a6671d39447fda045d873eba13840@app.getsentry.com/35914';
 module.exports.bing = 'Arzdiw4nlOJzRwOz__qailc8NiR31Tt51dN2D7cm57NrnceZnCpgOkmJhNpGoppU';
-module.exports.taskServer = 'http://54.147.184.23:8000/';
-module.exports.osmApi = 'https://www.openstreetmap.org/api/0.6/';
+
 module.exports.josm = 'http://localhost:8111/load_and_zoom?';
-module.exports.iD = 'http://openstreetmap.us/iD/release/#';
+
 module.exports.userProfileURL = 'https://www.openstreetmap.org/user/';
+
+// module.exports.taskServer = 'http://54.147.184.23:8000/';
+// module.exports.osmApi = 'https://www.openstreetmap.org/api/0.6/';
+// module.exports.iD = 'http://openstreetmap.us/iD/release/#';
+module.exports.taskServer = 'http://128.199.204.160:8000/';
+module.exports.osmApi = 'https://fast-dawn-4805.herokuapp.com/xml/';
+module.exports.iD = 'http://devseed.com/openroads-iD/';
